018665decbcd3258af4294174531e550226031f6
jcrademacher <johnjr5599@gmail.com>
2017-01-25 21:50:27 -0500

    created MainView.jsx, Content.jsx, and Panel.jsx. Added material-ui support to make app look like it wasnt made in 2000

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
-import Hello from './hello.jsx';
-import World from './world.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Content from "./components/Content.jsx"
+import Title from './components/Title.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MainView from './components/MainView.jsx'
+
+const App = () => (
+  <MuiThemeProvider>
+    <Content>
+      <Title/>
+      <MainView/>
+    </Content>
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
