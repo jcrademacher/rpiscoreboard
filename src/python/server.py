a1b94ba788c6cd275fe0eff26149de8fa09f9f32
jcrademacher <johnjr5599@gmail.com>
2017-02-20 18:18:41 -0500

    server.py needs multithreading for clock update

--- src/python/server.py
+++ src/python/server.py
@@ -289,7 +289,11 @@ try:
 		if x == 0:
 			#Wait forever for incoming htto requests
 			server.serve_forever()
-
+		
+		time.sleep(2)
+		
+		print "this code works"
+		
 		x += 1
 		if x == 0 or x % 5 == 0 and b.clockMode == "clock" and m != prevM:
 			b.serialWrite('H',chr(h),'M',chr(m),'S',chr(0))
