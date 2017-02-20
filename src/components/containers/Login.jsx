90dcc64b2244139d5d2c386c76db17ef4bfa24d4
jcrademacher <johnjr5599@gmail.com>
2017-02-19 22:02:44 -0500

    added sign in page on stats panel

---
+++ src/components/containers/Login.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Paper from "material-ui/Paper";
+import TextField from "material-ui/TextField";
+import FlatButton from "material-ui/FlatButton";
+
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		var styles = {
+			paper: {
+				backgroundColor: "#eaeaea",
+				height: "400",
+				width: "400",
+				margin: "auto"
+			},
+
+			username: {
+				fontSize: 20,
+			},
+
+			textField: {
+				width: "320"
+			},
+
+			buttons: {
+				width: "320",
+				height: "45",
+				margin: 10
+			}
+		};
+
+		return (
+			<div>
+				<p style={{fontSize: 60, margin: 40}}>Sign in</p>
+				<Paper style={styles.paper}>
+					<br/><br/><br/>
+					<form>
+						<TextField
+							hintText="Username..."
+							hintStyle={styles.username}
+							inputStyle={styles.username}
+							style={styles.textField}
+						/>
+						<br/><br/><br/>
+						<TextField
+							hintText="Password..."
+							hintStyle={styles.username}
+							inputStyle={styles.username}
+							style={styles.textField}
+							type="password"
+						/>
+						<br/><br/><br/>
+						<FlatButton
+							backgroundColor="#c1dad6"
+							label="Sign in"
+							labelStyle={{fontSize: 20}}
+							style={styles.buttons}
+						/>
+						<br/>
+						<FlatButton
+							label="Create new account"
+							backgroundColor="#E8D0A9"
+							labelStyle={{fontSize: 15}}
+							style={styles.buttons}
+						/>
+					</form>
+				</Paper>
+				<br/>
+				<br/>
+			</div>
+		);
+	}
+}
+
+const CenterView = () => {
+
+}
